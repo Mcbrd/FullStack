143d382d165942721fe6c25dd2874767e886026c
Brad <bradleymcmanus@hotmal.com>
2017-01-14 18:33:22 -0600

    goingButtons

--- GigHub/GigHub/Controllers/AttendeesController.cs
+++ GigHub/GigHub/Controllers/AttendeesController.cs
@@ -1,6 +1,8 @@
 ﻿using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System.Linq;
 using System.Web.Http;
+using GigHub.DTOs;
 
 namespace GigHub.Controllers
 {
@@ -14,12 +16,18 @@ namespace GigHub.Controllers
                 _context = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody]int gigId)
+        public IHttpActionResult Attend(AttendanceDto dto)
         {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attendance already exists");
+            
+
             var attendance = new Attendance
             {
-                GigId = gigId,
-                AttendeeId = User.Identity.GetUserId()
+                GigId = dto.GigId,
+                AttendeeId = userId
             };
             _context.Attendances.Add(attendance);
             _context.SaveChanges();
